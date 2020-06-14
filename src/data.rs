3a04d9e815529949ee8a7f29ab9e4ddd413f89ad
Navin Karkera <navin@disroot.org>
2020-06-14 13:34:51 +0530

    UPDATE: SAVING NOTES TO MARKDOWN FILES
    - Adding better support for mdbooks
    
    Signed-off by: Navin Karkera <navin@disroot.org>

--- src/data.rs
+++ src/data.rs
@@ -1,24 +1,63 @@
 use chrono::prelude::*;
 use colored::*;
 use serde::{Deserialize, Serialize};
-use std::fmt;
+use std::path::PathBuf;
+use std::{fmt, fs};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Note {
     datetime: DateTime<Local>,
+    directory: PathBuf,
+    title: String,
     note: String,
 }
 
 impl Note {
-    pub fn create(note: String) -> Note {
+    pub fn create(directory: PathBuf, note: String) -> Note {
+        let mut title = note.lines().next().unwrap().to_owned();
+        title = title.split("# ").collect();
         Note {
             datetime: Local::now(),
+            directory,
+            title,
             note,
         }
     }
 
-    pub fn contains(&self, keyword: &str) -> bool {
-        self.note.contains(keyword)
+    pub fn make_markdown(&self) -> String {
+        format!(
+            "---\ntitle: {}\ndate: {}\n\n---\n\n{}",
+            &self.title, &self.datetime, &self.note
+        )
+    }
+
+    pub fn update_summary(&self) -> std::io::Result<()> {
+        if let Ok(summary_file) = fs::read_to_string(&self.directory.join("SUMMARY.md")) {
+            let new_summary = format!(
+                "{}\n- [{}]({}.md)",
+                summary_file,
+                &self.title,
+                &self
+                    .title
+                    .split_whitespace()
+                    .collect::<Vec<&str>>()
+                    .join("_")
+            );
+            fs::write(&self.directory.join("SUMMARY.md"), new_summary)?
+        }
+        Ok(())
+    }
+    pub fn save(&self) -> std::io::Result<()> {
+        let note_path = &self.directory.join(format!(
+            "{}.md",
+            &self
+                .title
+                .split_whitespace()
+                .collect::<Vec<&str>>()
+                .join("_")
+        ));
+        fs::write(note_path, &self.make_markdown())?;
+        self.update_summary()
     }
 }
 
@@ -27,7 +66,7 @@ impl fmt::Display for Note {
         write!(
             f,
             "{}\n\n{} {}",
-            self.note.cyan(),
+            self.make_markdown().cyan(),
             "Created at:".green(),
             self.datetime.format("%v %r").to_string().magenta()
         )
